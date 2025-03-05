623708572ad4f1c066c3ecd0b5f9f2ccee9ba389
sebastian.marmol <sebastian.marmol@arppatechnologies.com>
2025-03-05 15:02:42 +0100

    Add Tailwind CSS configuration and VSCode settings

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      "flowbite-50": "#eff6ff",
+      "flowbite-100": "#dbeafe",
+      "flowbite-200": "#bfdbfe",
+      "flowbite-300": "#93c5fd",
+      "flowbite-400": "#60a5fa",
+      "flowbite-500": "#3b82f6",
+      "flowbite-600": "#2563eb",
+      "flowbite-700": "#1d4ed8",
+      "flowbite-800": "#1e40af",
+      "flowbite-900": "#1e3a8a",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
